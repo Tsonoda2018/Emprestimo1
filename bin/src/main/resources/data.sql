e1e332a17b91ddefb6a11b4b2611505402dd3fc5
Tsonoda2018 <tsonoda2003@hotmail.com>
2021-08-18 00:02:10 -0300

    teste
    testes

---
+++ bin/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into cliente (id, cpf, data_contratacao, nome, patrimonio, salario) values (null, '123', '2000-03-14', 'João Carlos da Silva', 235000.0, 7000.0)
+insert into cliente (id, cpf, data_contratacao, nome, patrimonio, salario) values (null, '234', '2020-08-13', 'Mariazinha', 35000.0, 5350.50)
+insert into cliente (id, cpf, data_contratacao, nome, patrimonio, salario) values (null, '345', '1997-05-23', 'Ricardo', 550000.0, 13355.0)
+
+insert into conta (id, agencia, cliente_id, numero, saldo) values (null, 1, 1, '2000-1', 3000.0)
+insert into conta (id, agencia, cliente_id, numero, saldo) values (null, 2, 2, '2001-2', 15500.0)
+insert into conta (id, agencia, cliente_id, numero, saldo) values (null, 3, 3, '2002-3', 300.0)
